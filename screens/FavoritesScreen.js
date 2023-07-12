67832e9edf101ea0afbab754a265bf7f3318fdd1
HIMANSHUGUPTA123456 <121235666+HIMANSHUGUPTA123456@users.noreply.github.com>
2023-07-13 04:47:48 +0530

    FavoritesScreen.js

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, ImageBackground, Image, ScrollView } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const FavoritesScreen = ({ navigation }) => {
@@ -26,25 +26,31 @@ const FavoritesScreen = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Here is the list of your favourite movies!!</Text>
-      {favorites.length === 0 ? (
-        <Text>No favorites yet</Text>
-      ) : (
-        <FlatList
-          data={favorites}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              style={styles.movieItem}
-              onPress={() => handleMoviePress(item)}
-            >
-              <Text style={styles.movieTitle}>{item.title}</Text>
-            </TouchableOpacity>
-          )}
-        />
-      )}
-    </View>
+    <ImageBackground source={require('./background.jpeg')} style={styles.backgroundImage}>
+      <ScrollView style={styles.container}>
+        <Text style={styles.title}>Here is the list of your favourite movies!!</Text>
+        {favorites.length === 0 ? (
+          <Text style={styles.movieTitle}>No favorites yet</Text>
+        ) : (
+          <View style={styles.moviesContainer}>
+            {favorites.map((movie) => (
+              <TouchableOpacity
+                style={styles.movieItem}
+                onPress={() => handleMoviePress(movie)}
+                key={movie.id.toString()}
+              >
+                <Image
+                  source={{ uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}` }}
+                  style={styles.moviePoster}
+                  resizeMode="contain"
+                />
+                <Text style={styles.movieTitle}>{movie.title}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        )}
+      </ScrollView>
+    </ImageBackground>
   );
 };
 
@@ -53,16 +59,36 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
   },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 10,
+    color: 'lightyellow',
+  },
+  moviesContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
   },
   movieItem: {
     marginBottom: 10,
+    width: '48%',
+  },
+  moviePoster: {
+    width: '100%',
+    aspectRatio: 2 / 3,
+    borderRadius: 5,
   },
   movieTitle: {
     fontSize: 16,
+    marginTop: 5,
+    textAlign: 'center',
+    color:'lightblue',
   },
 });
 
